1ce55dc62a3aa5eff09fbbfe48a3cb271f3f19c6
Oleh Shtembuliak <oleg.shtembulyack@gmail.com>
2025-05-28 10:42:53 +0800

    complete task 1

---
+++ js/task-1.js
@@ -0,0 +1,11 @@
+const categoriesList = document.querySelector('#categories');
+const categoryItems = categoriesList.querySelectorAll('li.item');
+
+console.log(`Number of categories: ${categoryItems.length}`);
+
+categoryItems.forEach(item => {
+  const categoryTitle = item.querySelector('h2').textContent;
+  const categoryElements = item.querySelectorAll('ul li');
+  console.log(`Category: ${categoryTitle}`);
+  console.log(`Elements: ${categoryElements.length}`);
+});

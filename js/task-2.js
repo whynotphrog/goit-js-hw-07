b3dda2c235b8a17944273155ca5453c2884a007d
Oleh Shtembuliak <oleg.shtembulyack@gmail.com>
2025-05-28 10:55:21 +0800

    complete task 2

--- js/task-2.js
+++ js/task-2.js
@@ -24,3 +24,12 @@ const images = [
     alt: "Lighthouse Coast Sea",
   }
 ];
+
+const gallery = document.querySelector('.gallery');
+const markup = images
+  .map(image => 
+    `<li class="gallery-item">
+      <img src="${image.url}" alt="${image.alt}" class="gallery-image">
+    </li>`)
+  .join('');
+gallery.insertAdjacentHTML('beforeend', markup);

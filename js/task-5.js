1c734a2dc838a6f43b2077541a928fe2af33c818
Oleh Shtembuliak <oleg.shtembulyack@gmail.com>
2025-05-28 11:04:35 +0800

    complete task 5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,12 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const changeColorBtn = document.querySelector('.change-color');
+const colorValueSpan = document.querySelector('.color');
+
+changeColorBtn.addEventListener('click', () => {
+  const newColor = getRandomHexColor();
+  document.body.style.backgroundColor = newColor;
+  colorValueSpan.textContent = newColor;
+});
